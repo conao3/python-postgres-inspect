2ebbe8ceef74638b49e4adf70c3bc7230a7f55a7
Naoya Yamashita <conao3@gmail.com>
2023-06-25 14:43:45 +0900

    add logging.conf

--- src/postgres_inspect/__main__.py
+++ src/postgres_inspect/__main__.py
@@ -4,3 +4,14 @@ from . import settings
 def main():
     print('hello world')
     print(settings.settings)
+
+    import logging.config
+    import pathlib
+    import yaml
+
+    with open(pathlib.Path() / 'logging.conf.yml', 'r') as f:
+        logging.config.dictConfig(yaml.safe_load(f))
+
+
+if __name__ == '__main__':
+    main()
